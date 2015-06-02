c53cd967464a9f6a12c497d0d7be71cfc6d8f1ac
Timotius Sitorus <timsitorus1@gmail.com>
2015-06-01 23:43:28 -0400

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,18 @@ var babelify = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
-gulp.task('selectionSort', function() {
-    browserify({
-    entries: './selectionSort/test.js',
-    debug: true
-    })
-    .transform(babelify)
-    .bundle()
-    .pipe(source('output.js'))
-    .pipe(gulp.dest('./selectionSort'))
-    .pipe(shell('node ./selectionSort/output.js'));
-});
+var sort = ['selectionSort', 'bubbleSort', 'selectionSort'];
 
-gulp.task('bubbleSort', function() {
-    browserify({
-    entries: './bubbleSort/test.js',
-    debug: true
-    })
-    .transform(babelify)
-    .bundle()
-    .pipe(source('output.js'))
-    .pipe(gulp.dest('./bubbleSort'))
-    .pipe(shell('node ./bubbleSort/output.js'));
+sort.map(function( sortName ) {
+    gulp.task(sortName, function() {
+        browserify({
+                entries: './'+ sortName +'/test.js',
+                debug: true
+            })
+            .transform(babelify)
+            .bundle()
+            .pipe(source('output.js'))
+            .pipe(gulp.dest('./'+ sortName +''))
+            .pipe(shell('node ./'+ sortName +'/output.js'));
+    });
 });
