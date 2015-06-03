ae4167faa82182490bd6311708211b84e7f0b599
Timotius Sitorus <timsitorus1@gmail.com>
2015-06-03 02:46:25 -0400

    testing

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,40 @@ var mocha = require('gulp-mocha')
 var babelify = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var Promise = require('bluebird');
 
 var sort = ['selectionSort', 'bubbleSort', 'insertionSort', 'mergeSort', 'quickSort'];
 
+var buildTask = function(sortName) {
+    return new Promise(function(resolve, reject) {
+        browserify({
+            entries: './'+ sortName +'/test.js',
+            debug: true
+        })
+        .transform(babelify)
+        .bundle()
+        .pipe(source('output.js'))
+        .pipe(gulp.dest('./'+ sortName +''))
+        .pipe(mocha({reporter: 'spec'}))
+        .pipe( function() {
+            resolve();
+        });
+    });
+}
+
 sort.map(function( sortName ) {
     gulp.task(sortName, function() {
-        browserify({
-                entries: './'+ sortName +'/test.js',
-                debug: true
-            })
-            .transform(babelify)
-            .bundle()
-            .pipe(source('output.js'))
-            .pipe(gulp.dest('./'+ sortName +''))
-            .pipe(mocha({reporter: 'spec'}));
+        buildTask(sortName)
     });
 });
 
 gulp.task('test', function() {
-
+    buildTask('selectionSort')
+        .then(buildTask('bubbleSort'))
+        .then(buildTask('insertionSort'))
+        .then(buildTask('mergeSort'))
+        .then(buildTask('quickSort'))
+        .catch(function(e) {
+            console.log(e);
+        });
 });
