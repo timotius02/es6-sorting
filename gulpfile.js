8c39d7cb181d92d0fd5992a4c6b5900ea515f1c9
Timotius Sitorus <timsitorus1@gmail.com>
2015-06-04 00:21:01 -0400

    added full test gulp command

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var mocha = require('gulp-mocha')
-
 var babelify = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
@@ -8,36 +8,50 @@ var Promise = require('bluebird');
 
 var sort = ['selectionSort', 'bubbleSort', 'insertionSort', 'mergeSort', 'quickSort'];
 
+gulp.task('test', function() {
+    Promise.map(sort, function(sortName) {
+            return buildTask(sortName);
+        }, {
+            concurrency: 1
+        })
+        .then(function() {
+            console.log("done");
+        });
+});
+
+
 var buildTask = function(sortName) {
     return new Promise(function(resolve, reject) {
         browserify({
-            entries: './'+ sortName +'/test.js',
-            debug: true
-        })
-        .transform(babelify)
-        .bundle()
-        .pipe(source('output.js'))
-        .pipe(gulp.dest('./'+ sortName +''))
-        .pipe(mocha({reporter: 'spec'}))
-        .pipe( function() {
-            resolve();
-        });
-    });
+                entries: './' + sortName + '/test.js',
+                debug: true
+            })
+            .transform(babelify)
+            .bundle()
+            .pipe(source('output.js'))
+            .pipe(gulp.dest('./' + sortName))
+            .pipe(mocha({
+                reporter: 'spec'
+            }))
+            .on('end', function() {
+                resolve('finish');
+            });
+    })
+
 }
 
-sort.map(function( sortName ) {
+sort.map(function(sortName) {
     gulp.task(sortName, function() {
         buildTask(sortName)
     });
 });
 
 gulp.task('test', function() {
-    buildTask('selectionSort')
-        .then(buildTask('bubbleSort'))
-        .then(buildTask('insertionSort'))
-        .then(buildTask('mergeSort'))
-        .then(buildTask('quickSort'))
-        .catch(function(e) {
-            console.log(e);
-        });
+    Promise.map(sort, function(sortName) {
+        return buildTask(sortName);
+    }, {
+        concurrency: 1
+    }).then(function() {
+        gutil.log(gutil.colors.green('Tests are Successful!'));
+    })
 });
